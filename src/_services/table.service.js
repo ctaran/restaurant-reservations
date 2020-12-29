9d01296efbb37de492ae2baecad660ca0e8c1503
Nicu Cristian Taran <cristian.taran.work@gmail.com>
2020-12-29 23:31:20 +0200

    frontend - multi-tenancy for Tables service

--- src/_services/table.service.js
+++ src/_services/table.service.js
@@ -1,4 +1,5 @@
 import { authHeader, handleResponse } from '../_helpers';
+import { authenticationService } from './authentication.service';
 
 export const tableService = {    
     getByRestaurantID,
@@ -8,7 +9,8 @@ export const tableService = {
 };
 
 async function getByRestaurantID(restaurant_id) {
-    const params = { restaurant_id: restaurant_id };
+    const manager_id = authenticationService.currentUserValue.id;
+    const params = { restaurant_id: restaurant_id, manager_id: manager_id  };
     const url = "/api/tables?" + Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&')
     const requestOptions = { method: 'GET', headers: authHeader() };
     const response = await fetch(url, requestOptions);
@@ -16,27 +18,38 @@ async function getByRestaurantID(restaurant_id) {
 }
 
 async function createNew(pos_x, pos_y, seats, restaurant_id) {
+    const manager_id = authenticationService.currentUserValue.id;
+    const params = { manager_id: manager_id  };
+    const url = "/api/table/new?" + Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&');
     const requestOptions = {
         method: 'POST', 
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ pos_x, pos_y, seats, restaurant_id })
+        body: JSON.stringify({ pos_x, pos_y, seats, restaurant_id, manager_id })
     }
-    const response = await fetch(`/api/table/new`, requestOptions);
+    const response = await fetch(url, requestOptions);
     return handleResponse(response);
 }
 
 async function updateByID(id, pos_x, pos_y, seats, restaurant_id) {
+    const manager_id = authenticationService.currentUserValue.id;
+    const params = { manager_id: manager_id  };
+    const url = `/api/table/${id}?` + Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&');
+
     const requestOptions = {
         method: 'PUT', 
         headers: Object.assign({}, authHeader(),  { "Content-Type": "application/json" }),
         body: JSON.stringify({ pos_x, pos_y, seats, restaurant_id }),
     }
-    const response = await fetch(`/api/table/${id}`, requestOptions);
+    const response = await fetch(url, requestOptions);
     return handleResponse(response);
 }
 
 async function deleteByID(id) {
+    const manager_id = authenticationService.currentUserValue.id;
+    const params = { manager_id: manager_id  };
+    const url = `/api/table/${id}?` + Object.keys(params).map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k])).join('&');
+
     const requestOptions = { method: 'DELETE', headers: authHeader() };
-    const response = await fetch(`/api/table/${id}`, requestOptions);
+    const response = await fetch(url, requestOptions);
     return handleResponse(response);
 }
